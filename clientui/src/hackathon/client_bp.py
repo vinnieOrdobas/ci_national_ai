94c886fa61498cc90b526c5fb6663739303fc5a6
ray <ray@dagon>
2024-08-29 18:33:31 +0100

    Connecting client ui to model

--- clientui/src/hackathon/client_bp.py
+++ clientui/src/hackathon/client_bp.py
@@ -1,14 +1,26 @@
 
+import time
+import os
+
 from flask import Blueprint
 from flask import render_template
 from flask import current_app
 from flask import request
 from flask import jsonify
-import time
+
+import vertexai
+from vertexai.preview.generative_models import GenerativeModel, ChatSession
 
 
 client_bp = Blueprint('clientui', __name__, template_folder='templates',
                       static_folder='static', url_prefix='/clientui')
+
+
+project_id = os.environ["PROJECT_ID"]
+location = os.environ["LOCATION"]
+endpoint_id = os.environ["ENDPOINT_ID"]
+
+vertexai.init(project=project_id, location=location)
 
 
 @client_bp.route("/", methods=['GET'])
@@ -20,5 +32,9 @@ def client_root():
 def send_prompt():
     data = request.json
     print("Got %s" % (data,))
-    time.sleep(1.5)
-    return jsonify({"reply": "dummy response for " + data['prompt']})
+
+    model = GenerativeModel(f"projects/{project_id}/locations/{location}/endpoints/{endpoint_id}")
+
+    response = model.generate_content([p['text'] for p in data['history']] + [data['prompt']])
+
+    return jsonify({"reply": response.text})
