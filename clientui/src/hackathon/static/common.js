8d6c458457776bebaed08893e8caea80b8e4bdbf
Sean <seanpatmeade@gmail.com>
2024-09-03 03:18:09 +0100

    add toggle and hise API token

--- clientui/src/hackathon/static/common.js
+++ clientui/src/hackathon/static/common.js
@@ -1,3 +1,12 @@
 function scroll_to(tag) {
     location.hash = "#" + tag;
 }
+
+function toggleDiv(id) {
+    var div = document.getElementById(id);
+    div.style.display = div.style.display == "none" ? "block" : "none";
+    div.classList.toggle("col-md-6");
+    var mapButton = document.getElementById("map-button");
+    mapButton.innerText = mapButton.innerText === "<" ? ">" : "<";
+    document.getElementById("chat-toggle").classList.toggle("col-md-6");
+  }
